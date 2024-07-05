48bc2ef7f185a4c5738b51ba8c0541d3fd5dad41
Simon Kye <kkeysimon@gmail.com>
2024-07-05 22:14:34 +0300

    portfolio update

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -1,27 +1,12 @@
 export default [
-    {
-        name: "room",
-        type: "glbModel",
-        path: "/models/roomFinaleFinale.glb",
-    },
-    {
-        name: "screen",
-        type: "videoTexture",
-        path: "/textures/portfolio.mp4",
-    },
-    {
-        name: "bpi",
-        type: "videoTexture",
-        path: "/textures/BPI.mp4",
-    },
-    {
-        name: "courseSearch",
-        type: "videoTexture",
-        path: "/textures/CourseSearch.mp4",
-    },
-    {
-        name: "artGuessr",
-        type: "videoTexture",
-        path: "/textures/ArtGuessr.mp4",
-    },
+  {
+    name: "room",
+    type: "glbModel",
+    path: "/models/roomFinaleFinale.glb",
+  },
+  {
+    name: "screen",
+    type: "videoTexture",
+    path: "/textures/portfolio.mp4",
+  },
 ];
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,143 +1,131 @@
 import Experience from "../Experience.js";
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper.js";
 import * as THREE from "three";
 import GSAP from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default class Room {
-    constructor() {
-        this.experience = new Experience();
-        this.scene = this.experience.scene;
-        this.resources = this.experience.resources;
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
-        //we know by the time world calls for new Room(),
-        //all assets must have loaded in order for World to trigger
-        //this means we can just call items.room as defined in assets.js
-        this.room = this.resources.items.room;
-        this.actualRoom = this.room.scene;
+    //we know by the time world calls for new Room(),
+    //all assets must have loaded in order for World to trigger
+    //this means we can just call items.room as defined in assets.js
+    this.room = this.resources.items.room;
+    this.actualRoom = this.room.scene;
 
-        this.lerp = {
-            current: 0,
-            target: 0,
-            ease: 0.1,
-        };
-        
-        this.setScrollTrigger();
-        this.setModel();
-        this.onMouseMove();
-    }
-    
-    setScrollTrigger() {
-        GSAP.registerPlugin(ScrollTrigger);
-        const switchMonitor = (str) => {
-            console.log(str);
-            this.actualRoom.children.forEach(child => {if (child.name === "Computer") {
-                child.children[1].material = new THREE.MeshBasicMaterial({
-                    map: this.resources.items[str],
-                });
-            }})
-        };
-        ScrollTrigger.create({
-            trigger: ".bpi",
-            start: "top center",
-            onEnter: () => switchMonitor("bpi"),
-            onEnterBack: () => switchMonitor("bpi"),
+    this.lerp = {
+      current: 0,
+      target: 0,
+      ease: 0.1,
+    };
+
+    this.setScrollTrigger();
+    this.setModel();
+    this.onMouseMove();
+  }
+
+  setScrollTrigger() {
+    GSAP.registerPlugin(ScrollTrigger);
+    const switchMonitor = (str) => {
+      console.log(str);
+      this.actualRoom.children.forEach((child) => {
+        if (child.name === "Computer") {
+          child.children[1].material = new THREE.MeshBasicMaterial({
+            map: this.resources.items[str],
+          });
+        }
+      });
+    };
+    // ScrollTrigger.create({
+    //     trigger: ".bpi",
+    //     start: "top center",
+    //     onEnter: () => switchMonitor("bpi"),
+    //     onEnterBack: () => switchMonitor("bpi"),
+    // });
+  }
+
+  setModel() {
+    this.actualRoom.children.forEach((child) => {
+      child.castShadow = true;
+      child.receiveShadow = true;
+
+      if (child instanceof THREE.Group) {
+        child.children.forEach((groupChild) => {
+          groupChild.castShadow = true;
+          groupChild.receiveShadow = true;
         });
-        ScrollTrigger.create({
-            trigger: ".courseSearch",
-            start: "top center",
-            onEnter: () => switchMonitor("courseSearch"),
-            onEnterBack: () => switchMonitor("courseSearch"),
+      }
 
+      console.log(child);
+
+      if (child.name === "Computer") {
+        console.log(this.resources.items.screen);
+        child.children[1].material = new THREE.MeshBasicMaterial({
+          map: this.resources.items.screen,
         });
+      }
 
-        ScrollTrigger.create({
-            trigger: ".artGuessr",
-            start: "top center",
-            onEnter: () => switchMonitor("artGuessr"),
-            onEnterBack: () => switchMonitor("artGuessr"),
-        });
-    }
+      if (child.name === "RugFloor") {
+        child.position.x = -0.50456;
+        child.position.y = 0.49075;
+        child.position.z = 2.9994;
+      }
 
-    setModel() {
-        
-        this.actualRoom.children.forEach(child => {
-            child.castShadow = true;
-            child.receiveShadow = true;
+      if (
+        child.name === "Flag" ||
+        child.name === "Floor_Items" ||
+        child.name === "Table" ||
+        child.name === "Bed" ||
+        child.name === "FloorSecond" ||
+        child.name === "FloorFirst" ||
+        child.name === "Shelf" ||
+        child.name === "Computer" ||
+        child.name === "Body" ||
+        child.name === "Cube"
+      ) {
+        child.scale.set(0, 0, 0);
+      }
+    });
 
-            if (child instanceof THREE.Group) {
-                child.children.forEach((groupChild) => {
-                    groupChild.castShadow = true;
-                    groupChild.receiveShadow = true;
-                });
-            } 
+    const width = 1;
+    const height = 1;
+    const intensity = 1;
+    const rectLight = new THREE.RectAreaLight(
+      0xffffff,
+      intensity,
+      width,
+      height
+    );
+    rectLight.position.set(-3.0178, 3.6586, 0.40913);
+    rectLight.rotation.y = Math.PI / 4 + Math.PI;
+    this.actualRoom.add(rectLight);
 
-            console.log(child);
+    //const rectLightHelper = new RectAreaLightHelper(rectLight);
 
-            if (child.name === "Computer") {
-                console.log(this.resources.items.screen)
-                child.children[1].material = new THREE.MeshBasicMaterial({
-                    map: this.resources.items.screen,
-                });
-            }
+    this.scene.add(this.actualRoom);
+    this.actualRoom.scale.set(0.3, 0.3, 0.3);
+  }
 
-            if (child.name === "RugFloor") {
-                child.position.x = -0.50456;
-                child.position.y = 0.49075;
-                child.position.z = 2.9994;
-            }
+  onMouseMove() {
+    window.addEventListener("mousemove", (e) => {
+      this.rotation =
+        ((e.clientX - window.innerWidth / 2) / window.innerWidth) * 2;
+      this.lerp.target = this.rotation * 0.2;
+    });
+  }
 
-            if (child.name === "Flag" ||
-                child.name === "Floor_Items" ||
-                child.name === "Table" ||
-                child.name === "Bed" ||
-                child.name === "FloorSecond" ||
-                child.name === "FloorFirst" ||
-                child.name === "Shelf" ||
-                child.name === "Computer" ||
-                child.name === "Body" ||
-                child.name === "Cube"
-            ) {
-                child.scale.set(0, 0, 0);
-            }
-            
-        });
+  resize() {}
 
-        const width = 1;
-        const height = 1;
-        const intensity = 1;
-        const rectLight = new THREE.RectAreaLight(
-            0xffffff,
-            intensity,
-            width,
-            height
-        );
-        rectLight.position.set(-3.0178, 3.6586, 0.40913);
-        rectLight.rotation.y = Math.PI / 4 + Math.PI;
-        this.actualRoom.add(rectLight);
+  update() {
+    this.lerp.current = GSAP.utils.interpolate(
+      this.lerp.current,
+      this.lerp.target,
+      this.lerp.ease
+    );
 
-        //const rectLightHelper = new RectAreaLightHelper(rectLight);
-
-        this.scene.add(this.actualRoom);
-        this.actualRoom.scale.set(0.3, 0.3, 0.3);   
-    }
-
-    onMouseMove() {
-        window.addEventListener("mousemove", (e) => {
-            this.rotation = (e.clientX - window.innerWidth / 2) / window.innerWidth * 2;
-            this.lerp.target = this.rotation * 0.2;
-        });
-    }
-
-    resize() {
-        
-    }
-
-    update() {
-        this.lerp.current = GSAP.utils.interpolate(
-            this.lerp.current, this.lerp.target, this.lerp.ease
-        );
-
-        this.actualRoom.rotation.y = this.lerp.current;
-    }
+    this.actualRoom.rotation.y = this.lerp.current;
+  }
 }
